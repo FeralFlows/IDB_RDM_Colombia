3665c151a2c27e1212b474bba4898ad1f35af974
FeralFlows <twild@umd.edu>
2020-03-18 12:00:24 -0400

    updated version of pic script that gathers results from separate uncertainty runs

--- pic/create_proj_file.R
+++ pic/create_proj_file.R
@@ -5,5 +5,5 @@ files <- list.files('../output/FinalRuns/IDB_RDM', pattern = "*.csv", full.names
 files
 # Note the "temp", saveProj = FALSE
 # this is to avoid saving a project file for each individual .csv in addition to all.proj
-proj <- mergeProjects("../output/FinalRuns/IDB_RDM/allResults.proj", lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
-# rgcam::addMIBatchCSV('../output/FinalRuns/IDB_RDM/all_results.csv', '../output/FinalRuns/IDB_RDM/results.dat')
+proj <- mergeProjects("../../output/FinalRuns/IDB_RDM/allResults.proj", lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
+# rgcam::addMIBatchCSV('../../output/FinalRuns/IDB_RDM/all_results.csv', '../output/FinalRuns/IDB_RDM/results.dat')
