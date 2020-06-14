f50969d701858f8f417bedce5c420325ee23431a
FeralFlows <twild@umd.edu>
2020-06-14 12:35:25 -0400

    removing unnecessary lines

--- pic/create_proj_file.R
+++ pic/create_proj_file.R
@@ -1,9 +1,4 @@
-#library('devtools')
-#install_github('JGCRI/rgcam', build_vignettes=TRUE)
 library('rgcam')
-files <- list.files('../output/FinalRuns/IDB_RDM', pattern = "*.csv", full.names=TRUE)
+files <- list.files('../output/FinalRuns/IDB_RDM/05242020', pattern = "*.csv", full.names=TRUE)
 files
-# Note the "temp", saveProj = FALSE
-# this is to avoid saving a project file for each individual .csv in addition to all.proj
-proj <- mergeProjects("../output/FinalRuns/IDB_RDM/allResults.proj", lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
-# rgcam::addMIBatchCSV('../output/FinalRuns/IDB_RDM/all_results.csv', '../output/FinalRuns/IDB_RDM/03202020.dat')
+proj <- mergeProjects("../output/FinalRuns/IDB_RDM/05242020/05242020.proj", lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
