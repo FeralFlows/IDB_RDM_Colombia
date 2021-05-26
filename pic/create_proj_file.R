8f567996dc4a08e6d8d4c8bbc04e14ace65432ac
FeralFlows <twild@umd.edu>
2021-05-26 10:37:55 -0400

    updating to reflect that outputs need to be stored in /pic/projects/GCAM/TomWild now to avoid storage capacity issues

--- pic/create_proj_file.R
+++ pic/create_proj_file.R
@@ -1,4 +1,7 @@
 library('rgcam')
-files <- list.files('../output/FinalRuns/IDB_RDM/05242020', pattern = "*.csv", full.names=TRUE)
-files
-proj <- mergeProjects("../output/FinalRuns/IDB_RDM/05242020/05242020.proj", lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
+files <- list.files('../output/FinalRuns/IDB_RDM/05252021', pattern = "*.csv", full.names=TRUE)
+#options("rgcam.saved_compressed" = FALSE)
+#files <- list.files('/pic/projects/GCAM/TomWild/output/FinalRuns/IDB_RDM/04052021', pattern = "*.csv", full.names=TRUE)
+#proj <- mergeProjects('/pic/projects/GCAM/TomWild/output/FinalRuns/IDB_RDM/04052021/QueryResults.proj', lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
+proj <- mergeProjects("../output/FinalRuns/IDB_RDM/05252021/QueryResults.proj", lapply(files, addMIBatchCSV, "temp", saveProj = FALSE))
+quit(save = "no")  # prevents pic from trying to save and reload workspaces, which sucks up time and memory
--- pic/create_query_proj_file.R
+++ pic/create_query_proj_file.R
@@ -1,6 +1,10 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-source('/qfs/people/wild566/IDB/Final/gcam-core-4/gcam-core/exe/proj_file_function.R')
-base_dir <- c('/qfs/people/wild566/IDB/Final/gcam-core-4/gcam-core/output/FinalRuns/IDB_RDM/05242020/')
-output_file <- c('05282020.proj')
+source('/qfs/people/wild566/IDB/Final/gcam-core/exe/proj_file_function.R')
+library('rgcam')
+options("rgcam.saved_compressed" = FALSE)
+#base_dir <- c('/qfs/people/wild566/IDB/Final/gcam-core/output/FinalRuns/IDB_RDM/04052021/')
+base_dir <- c('/pic/projects/GCAM/TomWild/output/FinalRuns/IDB_RDM/04052021/')
+output_file <- c('QueryResults.proj')
 produce_query_file(base_dir, output_file, proc_num=args[2], plotting_format=args[3])
+quit(save = "no")  # prevents pic from trying to save and reload workspaces, which sucks up time and memory
