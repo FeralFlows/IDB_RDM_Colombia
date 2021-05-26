d0f9d2140749f455f6ed823d583b9096efe5edc2
FeralFlows <twild@umd.edu>
2021-05-26 10:33:20 -0400

    updating which modules have to be loaded to conduct single gcam run successfully

--- pic/run-gcam.sh
+++ pic/run-gcam.sh
@@ -5,11 +5,11 @@
  
 job=$SLURM_JOB_NAME
 
-module load git
+module load git/2.7.0
 module load svn/1.8.13
 module load R/3.4.3
 module load java/1.8.0_31
-module load gcc/8.1.0
+module load gcc/6.1.0
  
 echo 'Library config:'
 ldd ./gcam.exe
