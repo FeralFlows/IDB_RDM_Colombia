b321e1f06bf5ff4e51e2be164a8862786dc8ecf6
wild566 <thomas.wild@pnnl.gov>
2021-08-20 13:18:34 -0700

    updating parallel arrays script to call modified log conf file to avoid log file collisions

--- relationships/gcam/scripts/run-gcam-parallel-arrays.sh
+++ relationships/gcam/scripts/run-gcam-parallel-arrays.sh
@@ -45,5 +45,5 @@ gcam_queries=$6
 echo "run gcam"
 date
 ldd ./gcam.exe
-echo "$xmldb_batch | sed "s#__OUTPUT_NAME__#${raw_outpath}${gcam_meta_scenario}_${NEW_TASK_ID}.csv#" | sed "s#__GCAM_Queries__#${gcam_queries}#" | ./gcam.exe -C$FILE -Llog_conf.xml"
-cat $xmldb_batch | sed "s#__OUTPUT_NAME__#${raw_outpath}${gcam_meta_scenario}_${NEW_TASK_ID}.csv#" | sed "s#__GCAM_Queries__#${gcam_queries}#" | ./gcam.exe -C$FILE -Llog_conf.xml
+echo "$xmldb_batch | sed "s#__OUTPUT_NAME__#${raw_outpath}${gcam_meta_scenario}_${NEW_TASK_ID}.csv#" | sed "s#__GCAM_Queries__#${gcam_queries}#" | ./gcam.exe -C$FILE -Llog_conf_mod.xml"
+cat $xmldb_batch | sed "s#__OUTPUT_NAME__#${raw_outpath}${gcam_meta_scenario}_${NEW_TASK_ID}.csv#" | sed "s#__GCAM_Queries__#${gcam_queries}#" | ./gcam.exe -C$FILE -Llog_conf_mod.xml
